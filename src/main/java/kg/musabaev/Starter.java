bc840fd29eef279a111ecffc7d5d8c75fe1c203d
Eldiyar Musabaev <musabaeveldiar@gmail.com>
2023-07-06 11:00:14 +0600

    add RabbitMQ component

--- src/main/java/kg/musabaev/Starter.java
+++ src/main/java/kg/musabaev/Starter.java
@@ -2,6 +2,7 @@ package kg.musabaev;
 
 import org.apache.camel.CamelContext;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.rabbitmq.RabbitMQComponent;
 import org.apache.camel.impl.DefaultCamelContext;
 
 import java.util.concurrent.TimeUnit;
@@ -10,12 +11,20 @@ public class Starter {
 	public static void main(String[] args) throws Exception {
 		CamelContext camel = new DefaultCamelContext();
 
+		var rabbitMq = new RabbitMQComponent();
+		rabbitMq.setHostname("localhost");
+		rabbitMq.setPortNumber(5672);
+		rabbitMq.setUsername("1");
+		rabbitMq.setPassword("1");
+		camel.addComponent("rabbitmq", rabbitMq);
+
 		camel.addRoutes(new RouteBuilder() {
 			@Override
 			public void configure() throws Exception {
 				from("timer:demo")
-						.setBody(simple("Hello world!"))
-						.log("${body}");
+						.setBody(simple("Hello from Camel!"))
+						.log("${body}")
+						.to("rabbitmq:d.test?queue=q.test&declare=true");
 			}
 		});
 		camel.start();
