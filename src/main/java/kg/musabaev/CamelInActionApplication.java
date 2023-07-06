8cf9c2ac6cc97077268f3f75a079576c883e142a
Eldiyar Musabaev <musabaeveldiar@gmail.com>
2023-07-06 19:35:18 +0600

    add camel-main module

--- src/main/java/kg/musabaev/CamelInActionApplication.java
+++ src/main/java/kg/musabaev/CamelInActionApplication.java
@@ -1,24 +1,22 @@
 package kg.musabaev;
 
-import org.apache.camel.CamelContext;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.rabbitmq.RabbitMQComponent;
-import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.camel.main.Main;
 
 import java.util.concurrent.TimeUnit;
 
-public class Starter {
+public class CamelInActionApplication {
 	public static void main(String[] args) throws Exception {
-		CamelContext camel = new DefaultCamelContext();
-
+		Main camel = new Main();
 		var rabbitMq = new RabbitMQComponent();
 		rabbitMq.setHostname("localhost");
 		rabbitMq.setPortNumber(5672);
 		rabbitMq.setUsername("1");
 		rabbitMq.setPassword("1");
-		camel.addComponent("rabbitmq", rabbitMq);
+		camel.bind("rabbitmq", rabbitMq);
 
-		camel.addRoutes(new RouteBuilder() {
+		camel.configure().addRoutesBuilder(new RouteBuilder() {
 			@Override
 			public void configure() throws Exception {
 				from("timer:demo")
@@ -27,8 +25,14 @@ public class Starter {
 						.to("rabbitmq:d.test?queue=q.test&declare=true");
 			}
 		});
-		camel.start();
+		new Thread(() -> {
+			try {
+				camel.run(args);
+			} catch (Exception e) {
+				throw new RuntimeException(e);
+			}
+		}).start();
 		TimeUnit.SECONDS.sleep(3);
-		camel.stop();
+		camel.completed();
 	}
 }
